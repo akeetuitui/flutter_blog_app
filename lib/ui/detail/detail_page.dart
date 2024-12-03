409a65af0753ab2701b2a6616c0b0cca031a3039
akeetuitui <skj6164@naver.com>
2024-12-03 15:22:54 +0900

    Initial commit

---
+++ lib/ui/detail/detail_page.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DetailPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('DetailPage'),
+    );
+  }
+
+}
---
+++ lib/ui/write/write_page.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+class WritePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('Writepage'),
+    );
+  }
+}
