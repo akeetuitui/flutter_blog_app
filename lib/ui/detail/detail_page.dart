2c63b40927419a1d7b547c96f62ef83f7a4328d2
akeetuitui <skj6164@naver.com>
2024-12-03 16:01:28 +0900

    detail: 앱바 버튼 구현

--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -8,10 +8,21 @@ class DetailPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+
       ),
       body: Text('DetailPage'),
     );
   }
 
+  Widget iconButton(IconData icon, void Function() onTap){ // 파라미터로 뭘 받을까 1. 아이콘 버튼이 달라짐 2. onTap 눌렀을 때 들어갈 함수들이 다름
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 50,
+        height: 50,
+        color: Colors.transparent,
+        child: Icon(icon),
+      ),
+    );
+  }
 }
