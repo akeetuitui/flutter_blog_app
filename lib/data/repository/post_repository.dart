916bd86b79857f376292a302e264b784dba3d247
akeetuitui <skj6164@naver.com>
2024-12-04 16:04:50 +0900

    post_repository: firebase 연결 부분 예외처리 (try, catch -> null 리턴)

--- lib/data/repository/post_repository.dart
+++ lib/data/repository/post_repository.dart
@@ -4,39 +4,42 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_firebase_blog_app/data/model/post.dart';
 
 class PostRepository {
-  Future<List<Post>> getAll() async {
+  Future<List<Post>?> getAll() async {
     // firestore에서 게시글 데이터를 가져오는 함수
     // 비동기 작업 - 데이터 가져오는 데 시간이 걸려도 앱이 멈추지 않음
-
-    // 1. 파이어스토어 인스턴스 가지고오기
-    final firestore = FirebaseFirestore.instance;
-    // 2. 컬렉션 참조 만들기
-    // posts라는 이름의 컬렉션에 게시글 데이터가 들어 있다고 가정하고, 참조를 만듦!
-    final collectionRef = firestore.collection('posts');
-    // 3. 값 불러오기
-    final result = await collectionRef.get();
-
-    final docs = result.docs; // docs라는 변수에 저장, 문서의 리스트로 반환
-    return docs.map((doc) {
-      final map = doc.data();
-      final newMap = {
-        'id': doc.id,
-        ...map, // 스프레드 연산자, 새로운 맵을 기존 맵에 합쳐서 풀어서 쓰기!
-      };
-      return Post.fromJson(newMap);
-    }).toList();
+    try {
+      // 1. 파이어스토어 인스턴스 가지고오기
+      final firestore = FirebaseFirestore.instance;
+      // 2. 컬렉션 참조 만들기
+      // posts라는 이름의 컬렉션에 게시글 데이터가 들어 있다고 가정하고, 참조를 만듦!
+      final collectionRef = firestore.collection('posts');
+      // 3. 값 불러오기
+      final result = await collectionRef.get();
+      final docs = result.docs; // docs라는 변수에 저장, 문서의 리스트로 반환
+      return docs.map((doc) {
+        final map = doc.data();
+        final newMap = {
+          'id': doc.id,
+          ...map, // 스프레드 연산자, 새로운 맵을 기존 맵에 합쳐서 풀어서 쓰기!
+        };
+        return Post.fromJson(newMap);
+      }).toList();
+    } catch (e) {
+      print(e); // Firebase 세팅에 에러가 났을 때, null 리턴!
+      return null;
+    }
   }
 
   // 1. Creat: 데이터 쓰기
-    Future<bool> insert({
-      required String title,
-      required String content,
-      required String writer,
-      required String imageUrl, 
-      // 비동기니까 Return 타입은 Future!
-      // Firestore의 'Posts' 컬렉션에 새 데이터 추가하는 함수
-    }) async { 
-      try{
+  Future<bool> insert({
+    required String title,
+    required String content,
+    required String writer,
+    required String imageUrl,
+    // 비동기니까 Return 타입은 Future!
+    // Firestore의 'Posts' 컬렉션에 새 데이터 추가하는 함수
+  }) async {
+    try {
       // 1) 파이어스토어 인스턴스 가지고오기
       final firestore = FirebaseFirestore.instance;
       // 2) 컬렉션 참조 만들기
@@ -51,12 +54,12 @@ class PostRepository {
         'imageUrl': imageUrl
       });
       return true; // 리턴타입에 bool, try문안에 잘 작동이 될 시 true
-      } catch(e){
-        print(e); 
-        // 에러가 났을 때 e 값 출력, 에러 내용 출력! 
-        return false; 
-      }
+    } catch (e) {
+      print(e);
+      // 에러가 났을 때 e 값 출력, 에러 내용 출력!
+      return false;
     }
+  }
 
   // 2. Read: 특정 ID로 하나의 도큐먼트 가져오기
 
