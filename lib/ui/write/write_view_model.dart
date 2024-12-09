70c77a603cd37959f1e220c8938f9c5bcbfa9ca3
akeetuitui <skj6164@naver.com>
2024-12-09 15:01:43 +0900

    사진 추가 뷰모델 수정

--- lib/ui/write/write_view_model.dart
+++ lib/ui/write/write_view_model.dart
@@ -1,19 +1,23 @@
 // 1. 상태 클래스 만들기
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_firebase_blog_app/data/model/post.dart';
 import 'package:flutter_firebase_blog_app/data/repository/post_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
 class WriteState {
   bool isWriting;
-  WriteState(this.isWriting);
+  String? imageUrl;
+  WriteState(this.isWriting, this.imageUrl);
 }
 
 // 2. 뷰모델 만들기
-
 class WriteViewModel extends AutoDisposeFamilyNotifier<WriteState, Post?> {
   @override
   WriteState build(Post? arg) {
-    return WriteState(false);
+    return WriteState(false, null);
   }
 
   Future<bool> insert({
@@ -21,19 +25,22 @@ class WriteViewModel extends AutoDisposeFamilyNotifier<WriteState, Post?> {
     required String title,
     required String content,
   }) async {
+    if (state.imageUrl == null) {
+      return false;
+    }
     final postRepository = PostRepository();
 
-    state = WriteState(true);
+    state = WriteState(true, null);
     if (arg == null) {
       // 포스트 객체가 널이면 : 새로작성
       final result = await postRepository.insert(
         title: title,
         content: content,
         writer: writer,
-        imageUrl: 'https://picsum.photos/200/300',
+        imageUrl: state.imageUrl!,
       );
       await Future.delayed(Duration(milliseconds: 500));
-      state = WriteState(false);
+      state = WriteState(false, null);
       return result;
     } else {
       // 널이 아니면 : 수정
@@ -42,28 +49,36 @@ class WriteViewModel extends AutoDisposeFamilyNotifier<WriteState, Post?> {
         writer: writer,
         title: title,
         content: content,
-        imageUrl: 'https://picsum.photos/200/300',
+        imageUrl: state.imageUrl!,
       );
       await Future.delayed(Duration(milliseconds: 500));
       return result;
     }
   }
 
-   void uploadImage() async{
-    // Firebase Storage 사용법
-    // 1. FirebaseStorage 객체 가지고 오기
-    // final storage = FirebaseStorage.instance;
-    // 2. 스토리치 참조 만들기
-    // Reference ref = storage.ref();
-    // 3. 파일 참조 만들기
-    // 4. 쓰기
-    // 5. 파일에 접근할 수 있는 URL 만들기
+  void uploadImage(XFile xFile) async {
+    try {
+      // Firebase Storage 사용법
+      // 1. FirebaseStorage 객체 가지고 오기
+      final storage = FirebaseStorage.instance;
+      // 2. 스토리치 참조 만들기
+      Reference ref = storage.ref();
+      // 3. 파일 참조 만들기
+      Reference fileRef =
+          ref.child('${DateTime.now().microsecondsSinceEpoch}_${xFile.name}');
+      // 4. 쓰기!
+      await fileRef.putFile(File(xFile.path));
+      // 5. 파일에 접근할 수 있는 URL 만들기
+      String imageUrl = await fileRef.getDownloadURL();
+      state = WriteState(state.isWriting, imageUrl);
+    } catch (e) {
+      print(e);
+    }
   }
 }
 
 // 3. 뷰모델 관리자 만들기
-final writeViewModelProvider = NotifierProvider.autoDispose.family<WriteViewModel, WriteState, Post?>(
-  (){
-    return WriteViewModel();
-  }
-);
+final writeViewModelProvider =
+    NotifierProvider.autoDispose.family<WriteViewModel, WriteState, Post?>(() {
+  return WriteViewModel();
+});
